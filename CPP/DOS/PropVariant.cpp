dajhorn/retro7zip

commit 89b1970410d70bf651f6bf8a6b8d1a7ebf6d2a77

Prune and normalize CPP/DOS files.
Improve readability, remove non-DOS cruft, and fix spelling mistakes.

CPP/DOS/PropVariant.cpp
@@ -1,13 +1,12 @@
-// Windows/PropVariant.cpp
+// 7-Zip PropVariant.cpp for DOS.
 
 #include "StdAfx.h"
 
-#include "../Common/Defs.h"
-
-#include "PropVariant.h"
-
 namespace NDOS {
 namespace NCOM {
+
+static const char * const kMemException = "out of memory";
+
 
 BSTR AllocBstrFromAscii(const char *s) throw()
 {
@@ -15,13 +14,13 @@ BSTR AllocBstrFromAscii(const char *s) throw()
     return NULL;
   UINT len = (UINT)strlen(s);
   BSTR p = ::SysAllocStringLen(NULL, len);
-  if (p)
-  {
+  if (p) {
     for (UINT i = 0; i <= len; i++)
       p[i] = (Byte)s[i];
   }
   return p;
 }
+
 
 HRESULT PropVarEm_Alloc_Bstr(PROPVARIANT *p, unsigned numChars) throw()
 {
@@ -36,6 +35,7 @@ HRESULT PropVarEm_Alloc_Bstr(PROPVARIANT *p, unsigned numChars) throw()
   return S_OK;
 }
 
+
 HRESULT PropVarEm_Set_Str(PROPVARIANT *p, const char *s) throw()
 {
   p->bstrVal = AllocBstrFromAscii(s);
@@ -49,11 +49,13 @@ HRESULT PropVarEm_Set_Str(PROPVARIANT *p, const char *s) throw()
   return E_OUTOFMEMORY;
 }
 
+
 CPropVariant::CPropVariant(const PROPVARIANT &varSrc)
 {
   vt = VT_EMPTY;
   InternalCopy(&varSrc);
 }
+
 
 CPropVariant::CPropVariant(const CPropVariant &varSrc)
 {
@@ -61,11 +63,13 @@ CPropVariant::CPropVariant(const CPropVariant &varSrc)
   InternalCopy(&varSrc);
 }
 
+
 CPropVariant::CPropVariant(BSTR bstrSrc)
 {
   vt = VT_EMPTY;
   *this = bstrSrc;
 }
+
 
 CPropVariant::CPropVariant(LPCOLESTR lpszSrc)
 {
@@ -73,11 +77,13 @@ CPropVariant::CPropVariant(LPCOLESTR lpszSrc)
   *this = lpszSrc;
 }
 
+
 CPropVariant& CPropVariant::operator=(const CPropVariant &varSrc)
 {
   InternalCopy(&varSrc);
   return *this;
 }
+
 
 CPropVariant& CPropVariant::operator=(const PROPVARIANT &varSrc)
 {
@@ -85,13 +91,13 @@ CPropVariant& CPropVariant::operator=(const PROPVARIANT &varSrc)
   return *this;
 }
 
+
 CPropVariant& CPropVariant::operator=(BSTR bstrSrc)
 {
   *this = (LPCOLESTR)bstrSrc;
   return *this;
 }
 
-static const char * const kMemException = "out of memory";
 
 CPropVariant& CPropVariant::operator=(LPCOLESTR lpszSrc)
 {
@@ -108,6 +114,7 @@ CPropVariant& CPropVariant::operator=(LPCOLESTR lpszSrc)
   return *this;
 }
 
+
 CPropVariant& CPropVariant::operator=(const UString &s)
 {
   InternalClear();
@@ -119,36 +126,18 @@ CPropVariant& CPropVariant::operator=(const UString &s)
   return *this;
 }
 
+
 CPropVariant& CPropVariant::operator=(const UString2 &s)
 {
-  /*
-  if (s.IsEmpty())
-    *this = L"";
-  else
-  */
-  {
-    InternalClear();
-    vt = VT_BSTR;
-    wReserved1 = 0;
-    bstrVal = ::SysAllocStringLen(s.GetRawPtr(), s.Len());
-    if (!bstrVal)
-      throw kMemException;
-    /* SysAllocStringLen probably appends a null-terminating character for NULL string.
-       But it doesn't specified in MSDN.
-       But we suppose that it works
-
-    if (!s.GetRawPtr())
-    {
-      *bstrVal = 0;
-    }
-    */
-
-    /* MSDN: Windows CE: SysAllocStringLen() : Passing invalid (and under some circumstances NULL)
-                         pointers to this function causes  an unexpected termination of the application.
-       Is it safe? Maybe we must chamnge the code for that case ? */
-  }
+  InternalClear();
+  vt = VT_BSTR;
+  wReserved1 = 0;
+  bstrVal = ::SysAllocStringLen(s.GetRawPtr(), s.Len());
+  if (!bstrVal)
+    throw kMemException;
   return *this;
 }
+
 
 CPropVariant& CPropVariant::operator=(const char *s)
 {
@@ -165,6 +154,7 @@ CPropVariant& CPropVariant::operator=(const char *s)
   return *this;
 }
 
+
 CPropVariant& CPropVariant::operator=(bool bSrc) throw()
 {
   if (vt != VT_BOOL)
@@ -175,6 +165,7 @@ CPropVariant& CPropVariant::operator=(bool bSrc) throw()
   boolVal = bSrc ? VARIANT_TRUE : VARIANT_FALSE;
   return *this;
 }
+
 
 BSTR CPropVariant::AllocBstr(unsigned numChars)
 {
@@ -192,22 +183,27 @@ BSTR CPropVariant::AllocBstr(unsigned numChars)
   return bstrVal;
 }
 
+
 #define SET_PROP_id_dest(id, dest) \
   if (vt != id) { InternalClear(); vt = id; } dest = value; wReserved1 = 0;
+
 
 void CPropVariant::Set_Int32(Int32 value) throw()
 {
   SET_PROP_id_dest(VT_I4, lVal)
 }
 
+
 void CPropVariant::Set_Int64(Int64 value) throw()
 {
   SET_PROP_id_dest(VT_I8, hVal.QuadPart)
 }
 
+
 #define SET_PROP_FUNC(type, id, dest) \
   CPropVariant& CPropVariant::operator=(type value) throw() \
   { SET_PROP_id_dest(id, dest)  return *this; }
+
 
 SET_PROP_FUNC(Byte, VT_UI1, bVal)
 // SET_PROP_FUNC(Int16, VT_I2, iVal)
@@ -244,7 +240,7 @@ SET_PROP_FUNC(const FILETIME &, VT_FILETIME, filetime)
   So we handle VT_FILETIME and another simple types directly
   we call system functions for VT_BSTR and for unknown typed
 */
- 
+
 CPropVariant::~CPropVariant() throw()
 {
   switch ((unsigned)vt)
@@ -252,10 +248,12 @@ CPropVariant::~CPropVariant() throw()
     CASE_SIMPLE_VT_VALUES
       // vt = VT_EMPTY; // it's optional
       return;
-    default: break;
+    default:
+      break;
   }
   ::VariantClear((tagVARIANT *)this);
 }
+
 
 HRESULT PropVariant_Clear(PROPVARIANT *prop) throw()
 {
@@ -265,12 +263,10 @@ HRESULT PropVariant_Clear(PROPVARIANT *prop) throw()
       prop->vt = VT_EMPTY;
       break;
     default:
-    {
       const HRESULT res = ::VariantClear((VARIANTARG *)prop);
       if (res != S_OK || prop->vt != VT_EMPTY)
         return res;
       break;
-    }
   }
   prop->wReserved1 = 0;
   prop->wReserved2 = 0;
@@ -278,6 +274,7 @@ HRESULT PropVariant_Clear(PROPVARIANT *prop) throw()
   prop->uhVal.QuadPart = 0;
   return S_OK;
 }
+
 
 HRESULT CPropVariant::Clear() throw()
 {
@@ -289,6 +286,7 @@ HRESULT CPropVariant::Clear() throw()
   return PropVariant_Clear(this);
 }
 
+
 HRESULT CPropVariant::Copy(const PROPVARIANT* pSrc) throw()
 {
   Clear();
@@ -297,7 +295,8 @@ HRESULT CPropVariant::Copy(const PROPVARIANT* pSrc) throw()
     CASE_SIMPLE_VT_VALUES
       memmove((PROPVARIANT*)this, pSrc, sizeof(PROPVARIANT));
       return S_OK;
-    default: break;
+    default:
+      break;
   }
   return ::VariantCopy((tagVARIANT *)this, (tagVARIANT *)const_cast<PROPVARIANT *>(pSrc));
 }
@@ -308,12 +307,12 @@ HRESULT CPropVariant::Attach(PROPVARIANT *pSrc) throw()
   const HRESULT hr = Clear();
   if (FAILED(hr))
     return hr;
-  // memcpy((PROPVARIANT *)this, pSrc, sizeof(PROPVARIANT));
   *(PROPVARIANT *)this = *pSrc;
   pSrc->vt = VT_EMPTY;
   pSrc->wReserved1 = 0;
   return S_OK;
 }
+
 
 HRESULT CPropVariant::Detach(PROPVARIANT *pDest) throw()
 {
@@ -323,12 +322,12 @@ HRESULT CPropVariant::Detach(PROPVARIANT *pDest) throw()
     if (FAILED(hr))
       return hr;
   }
-  // memcpy(pDest, this, sizeof(PROPVARIANT));
   *pDest = *(PROPVARIANT *)this;
   vt = VT_EMPTY;
   wReserved1 = 0;
   return S_OK;
 }
+
 
 HRESULT CPropVariant::InternalClear() throw()
 {
@@ -345,6 +344,7 @@ HRESULT CPropVariant::InternalClear() throw()
   }
   return hr;
 }
+
 
 void CPropVariant::InternalCopy(const PROPVARIANT *pSrc)
 {
@@ -363,31 +363,58 @@ int CPropVariant::Compare(const CPropVariant &a) throw()
 {
   if (vt != a.vt)
     return MyCompare(vt, a.vt);
+
   switch ((unsigned)vt)
   {
-    case VT_EMPTY: return 0;
-    // case VT_I1: return MyCompare(cVal, a.cVal);
-    case VT_UI1: return MyCompare(bVal, a.bVal);
-    case VT_I2: return MyCompare(iVal, a.iVal);
-    case VT_UI2: return MyCompare(uiVal, a.uiVal);
-    case VT_I4: return MyCompare(lVal, a.lVal);
-    case VT_UI4: return MyCompare(ulVal, a.ulVal);
-    // case VT_UINT: return MyCompare(uintVal, a.uintVal);
-    case VT_I8: return MyCompare(hVal.QuadPart, a.hVal.QuadPart);
-    case VT_UI8: return MyCompare(uhVal.QuadPart, a.uhVal.QuadPart);
-    case VT_BOOL: return -MyCompare(boolVal, a.boolVal);
+    case VT_EMPTY:
+      return 0;
+
+//  case VT_I1:
+//    return MyCompare(cVal, a.cVal);
+
+    case VT_UI1:
+      return MyCompare(bVal, a.bVal);
+
+    case VT_I2:
+      return MyCompare(iVal, a.iVal);
+
+    case VT_UI2:
+      return MyCompare(uiVal, a.uiVal);
+
+    case VT_I4:
+      return MyCompare(lVal, a.lVal);
+
+    case VT_UI4:
+      return MyCompare(ulVal, a.ulVal);
+
+//  case VT_UINT:
+//    return MyCompare(uintVal, a.uintVal);
+
+    case VT_I8:
+      return MyCompare(hVal.QuadPart, a.hVal.QuadPart);
+
+    case VT_UI8:
+      return MyCompare(uhVal.QuadPart, a.uhVal.QuadPart);
+
+    case VT_BOOL:
+      return -MyCompare(boolVal, a.boolVal);
+
     case VT_FILETIME:
-    {
       const int res = CompareFileTime(&filetime, &a.filetime);
       if (res != 0)
         return res;
       const unsigned v1 = Get_Ns100();
       const unsigned v2 = a.Get_Ns100();
       return MyCompare(v1, v2);
-    }
-    case VT_BSTR: return 0; // Not implemented
-    default: return 0;
+
+    case VT_BSTR:
+      // Not implemented
+      return 0;
+
+    default:
+      return 0;
   }
 }
+
 
 }}

CPP/DOS/PropVariantConv.cpp
@@ -1,11 +1,7 @@
-// 7-Zip PropVariantConv.cpp for DOS
+// 7-Zip PropVariantConv.cpp for DOS.
 
 #include "StdAfx.h"
-
 #include "../Common/IntToString.h"
-
-#include "Defs.h"
-#include "PropVariantConv.h"
 
 #define UINT_TO_STR_2(c, val) { s[0] = (c); s[1] = (char)('0' + (val) / 10); s[2] = (char)('0' + (val) % 10); s += 3; }
 
@@ -25,7 +21,9 @@ bool ConvertUtcFileTimeToString2(const FILETIME &utc, unsigned ns100, char *s, i
       g_Timestamp_Show_UTC;
 
   if (show_utc)
+  {
     ft = utc;
+  }
   else
   {
     if (!FileTimeToLocalFileTime(&utc, &ft))
@@ -34,10 +32,7 @@ bool ConvertUtcFileTimeToString2(const FILETIME &utc, unsigned ns100, char *s, i
 
   SYSTEMTIME st;
   if (!BOOLToBool(FileTimeToSystemTime(&ft, &st)))
-  {
-    // win10 : that function doesn't work, if bit 63 of 64-bit FILETIME is set.
     return false;
-  }
 
   {
     unsigned val = st.wYear;
@@ -52,15 +47,16 @@ bool ConvertUtcFileTimeToString2(const FILETIME &utc, unsigned ns100, char *s, i
     s[0] = (char)('0' + val / 10);
     s += 4;
   }
+
   UINT_TO_STR_2('-', st.wMonth)
   UINT_TO_STR_2('-', st.wDay)
-  
+
   if (level > kTimestampPrintLevel_DAY)
   {
     const char setChar = ' ';
     UINT_TO_STR_2(setChar, st.wHour)
     UINT_TO_STR_2(':', st.wMinute)
-    
+
     if (level >= kTimestampPrintLevel_SEC)
     {
       UINT_TO_STR_2(':', st.wSecond)
@@ -68,17 +64,7 @@ bool ConvertUtcFileTimeToString2(const FILETIME &utc, unsigned ns100, char *s, i
       if (level > kTimestampPrintLevel_SEC)
       {
         *s++ = '.';
-        /*
-        {
-          unsigned val = st.wMilliseconds;
-          s[2] = (char)('0' + val % 10); val /= 10;
-          s[1] = (char)('0' + val % 10);
-          s[0] = (char)('0' + val / 10);
-          s += 3;
-        }
-        *s++ = ' ';
-        */
-        
+
         {
           unsigned numDigits = 7;
           UInt32 val = (UInt32)((((UInt64)ft.dwHighDateTime << 32) + ft.dwLowDateTime) % 10000000);
@@ -91,6 +77,7 @@ bool ConvertUtcFileTimeToString2(const FILETIME &utc, unsigned ns100, char *s, i
             numDigits = (unsigned)level;
           s += numDigits;
         }
+
         if (level >= kTimestampPrintLevel_NTFS + 1)
         {
           *s++ = (char)('0' + (ns100 / 10));
@@ -100,7 +87,7 @@ bool ConvertUtcFileTimeToString2(const FILETIME &utc, unsigned ns100, char *s, i
       }
     }
   }
-  
+
   if (show_utc)
   {
     if ((flags & kTimestampPrintFlags_DisableZ) == 0)
@@ -117,6 +104,7 @@ bool ConvertUtcFileTimeToString(const FILETIME &utc, char *s, int level) throw()
   return ConvertUtcFileTimeToString2(utc, 0, s, level);
 }
 
+
 bool ConvertUtcFileTimeToString2(const FILETIME &ft, unsigned ns100, wchar_t *dest, int level) throw()
 {
   char s[k_TimeStringBufferSize];
@@ -130,6 +118,7 @@ bool ConvertUtcFileTimeToString2(const FILETIME &ft, unsigned ns100, wchar_t *de
   }
   return res;
 }
+
 
 bool ConvertUtcFileTimeToString(const FILETIME &ft, wchar_t *dest, int level) throw()
 {
@@ -151,63 +140,124 @@ void ConvertPropVariantToShortString(const PROPVARIANT &prop, char *dest) throw(
   *dest = 0;
   switch (prop.vt)
   {
-    case VT_EMPTY: return;
-    case VT_BSTR: dest[0] = '?'; dest[1] = 0; return;
-    case VT_UI1: ConvertUInt32ToString(prop.bVal, dest); return;
-    case VT_UI2: ConvertUInt32ToString(prop.uiVal, dest); return;
-    case VT_UI4: ConvertUInt32ToString(prop.ulVal, dest); return;
-    case VT_UI8: ConvertUInt64ToString(prop.uhVal.QuadPart, dest); return;
+    case VT_EMPTY:
+      return;
+
+    case VT_BSTR:
+      dest[0] = '?';
+      dest[1] = 0;
+      return;
+
+    case VT_UI1:
+      ConvertUInt32ToString(prop.bVal, dest);
+      return;
+
+    case VT_UI2:
+      ConvertUInt32ToString(prop.uiVal, dest);
+      return;
+
+    case VT_UI4:
+      ConvertUInt32ToString(prop.ulVal, dest);
+      return;
+
+    case VT_UI8:
+      ConvertUInt64ToString(prop.uhVal.QuadPart, dest);
+      return;
+
     case VT_FILETIME:
-    {
-      // const unsigned prec = prop.wReserved1;
       int level = 0;
-      /*
-      if (prec == 0)
-        level = 7;
-      else if (prec > 16 && prec <= 16 + 9)
-        level = prec - 16;
-      */
       ConvertUtcFileTimeToString(prop.filetime, dest, level);
       return;
-    }
-    // case VT_I1: return ConvertInt64ToString(prop.cVal, dest); return;
-    case VT_I2: ConvertInt64ToString(prop.iVal, dest); return;
-    case VT_I4: ConvertInt64ToString(prop.lVal, dest); return;
-    case VT_I8: ConvertInt64ToString(prop.hVal.QuadPart, dest); return;
-    case VT_BOOL: dest[0] = VARIANT_BOOLToBool(prop.boolVal) ? '+' : '-'; dest[1] = 0; return;
-    default: dest[0] = '?'; dest[1] = ':'; ConvertUInt64ToString(prop.vt, dest + 2);
+
+//  case VT_I1:
+//    ConvertInt64ToString(prop.cVal, dest);
+//    return;
+
+    case VT_I2:
+      ConvertInt64ToString(prop.iVal, dest);
+      return;
+
+    case VT_I4:
+      ConvertInt64ToString(prop.lVal, dest);
+      return;
+
+    case VT_I8:
+      ConvertInt64ToString(prop.hVal.QuadPart, dest);
+      return;
+
+    case VT_BOOL:
+      dest[0] = VARIANT_BOOLToBool(prop.boolVal) ? '+' : '-';
+      dest[1] = 0;
+      return;
+
+    default:
+      dest[0] = '?';
+      dest[1] = ':';
+      ConvertUInt64ToString(prop.vt, dest + 2);
+	  return;
   }
 }
+
 
 void ConvertPropVariantToShortString(const PROPVARIANT &prop, wchar_t *dest) throw()
 {
   *dest = 0;
   switch (prop.vt)
   {
-    case VT_EMPTY: return;
-    case VT_BSTR: dest[0] = '?'; dest[1] = 0; return;
-    case VT_UI1: ConvertUInt32ToString(prop.bVal, dest); return;
-    case VT_UI2: ConvertUInt32ToString(prop.uiVal, dest); return;
-    case VT_UI4: ConvertUInt32ToString(prop.ulVal, dest); return;
-    case VT_UI8: ConvertUInt64ToString(prop.uhVal.QuadPart, dest); return;
+    case VT_EMPTY:
+      return;
+
+    case VT_BSTR:
+      dest[0] = '?';
+      dest[1] = 0;
+      return;
+
+    case VT_UI1:
+      ConvertUInt32ToString(prop.bVal, dest);
+      return;
+
+    case VT_UI2:
+      ConvertUInt32ToString(prop.uiVal, dest);
+      return;
+
+    case VT_UI4:
+      ConvertUInt32ToString(prop.ulVal, dest);
+      return;
+
+    case VT_UI8:
+      ConvertUInt64ToString(prop.uhVal.QuadPart, dest);
+      return;
+
     case VT_FILETIME:
-    {
-      // const unsigned prec = prop.wReserved1;
       int level = 0;
-      /*
-      if (prec == 0)
-        level = 7;
-      else if (prec > 16 && prec <= 16 + 9)
-        level = prec - 16;
-      */
       ConvertUtcFileTimeToString(prop.filetime, dest, level);
       return;
-    }
-    // case VT_I1: return ConvertInt64ToString(prop.cVal, dest); return;
-    case VT_I2: ConvertInt64ToString(prop.iVal, dest); return;
-    case VT_I4: ConvertInt64ToString(prop.lVal, dest); return;
-    case VT_I8: ConvertInt64ToString(prop.hVal.QuadPart, dest); return;
-    case VT_BOOL: dest[0] = VARIANT_BOOLToBool(prop.boolVal) ? (wchar_t)'+' : (wchar_t)'-'; dest[1] = 0; return;
-    default: dest[0] = '?'; dest[1] = ':'; ConvertUInt32ToString(prop.vt, dest + 2);
+
+//  case VT_I1:
+//    ConvertInt64ToString(prop.cVal, dest);
+//    return;
+
+    case VT_I2:
+      ConvertInt64ToString(prop.iVal, dest);
+      return;
+
+    case VT_I4:
+      ConvertInt64ToString(prop.lVal, dest);
+      return;
+
+    case VT_I8:
+      ConvertInt64ToString(prop.hVal.QuadPart, dest);
+      return;
+
+    case VT_BOOL:
+      dest[0] = VARIANT_BOOLToBool(prop.boolVal) ? (wchar_t)'+' : (wchar_t)'-';
+      dest[1] = 0;
+      return;
+
+    default:
+      dest[0] = '?';
+      dest[1] = ':';
+      ConvertUInt32ToString(prop.vt, dest + 2);
+	  return;
   }
 }

CPP/DOS/Defs.h
@@ -5,9 +5,24 @@
 
 #include "../Common/MyWindows.h"
 
-inline bool BOOLToBool(BOOL v) { return (v != FALSE); }
 
-inline VARIANT_BOOL BoolToVARIANT_BOOL(bool v) { return (v ? VARIANT_TRUE: VARIANT_FALSE); }
-inline bool VARIANT_BOOLToBool(VARIANT_BOOL v) { return (v != VARIANT_FALSE); }
+inline bool BOOLToBool(BOOL v)
+{
+  return (v != FALSE);
+}
+
+
+inline VARIANT_BOOL BoolToVARIANT_BOOL(bool v)
+{
+  return (v ? VARIANT_TRUE: VARIANT_FALSE);
+}
+
+
+inline bool VARIANT_BOOLToBool(VARIANT_BOOL v)
+{
+  return (v != VARIANT_FALSE);
+}
+
 
 #endif // ZIP7_INC_DOS_DEFS_H
+

CPP/DOS/ErrorMsg.cpp
@@ -1,13 +1,12 @@
 // 7-Zip ErrorMsg.h for DOS
 
 #include "StdAfx.h"
-
 #include "../Common/StringConvert.h"
 
-#include "ErrorMsg.h"
 
 namespace NDOS {
 namespace NError {
+
 
 static bool MyFormatMessage(DWORD errorCode, UString &message)
 {
@@ -70,6 +69,7 @@ static bool MyFormatMessage(DWORD errorCode, UString &message)
   return true;
 }
 
+
 UString MyFormatMessage(DWORD errorCode)
 {
   UString m;
@@ -92,5 +92,6 @@ UString MyFormatMessage(DWORD errorCode)
     m.DeleteFrom(m.Len() - 2);
   return m;
 }
+
 
 }}

CPP/DOS/ErrorMsg.h
@@ -1,4 +1,4 @@
-// Windows/ErrorMsg.h
+// 7-Zip ErrorMsg.h for DOS.
 
 #ifndef ZIP7_INC_DOS_ERROR_MSG_H
 #define ZIP7_INC_DOS_ERROR_MSG_H
@@ -8,8 +8,15 @@
 namespace NDOS {
 namespace NError {
 
+
 UString MyFormatMessage(DWORD errorCode);
-inline UString MyFormatMessage(HRESULT errorCode) { return MyFormatMessage((DWORD)errorCode); }
+
+
+inline UString MyFormatMessage(HRESULT errorCode)
+{
+  return MyFormatMessage((DWORD)errorCode);
+}
+
 
 }}
 

CPP/DOS/FileDir.cpp
@@ -1,30 +1,12 @@
 // 7-Zip FileDir.cpp for DOS
 
 #include "StdAfx.h"
-
-
-#include <dos.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <limits.h>
-#include <unistd.h>
-#include <time.h>
-#include <utime.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-
-#include "../Common/StringConvert.h"
-#include "../Common/C_FileIO.h"
-
-#include "FileDir.h"
-#include "FileFind.h"
-#include "FileName.h"
 
 using namespace NDOS;
 using namespace NFile;
 using namespace NName;
+
 
 static bool FiTime_To_timespec(const CFiTime *ft, timespec &ts)
 {
@@ -47,13 +29,17 @@ static bool FiTime_To_timespec(const CFiTime *ft, timespec &ts)
   }
 }
 
+
 namespace NDOS {
 namespace NFile {
 namespace NDir {
 
 
+bool MyGetFullPathName(CFSTR path, FString &resFullPath)
+{
+  return GetFullPath(path, resFullPath);
+}
 
-static bool CreateDir2(CFSTR path);
 
 bool CreateComplexDir(CFSTR _path)
 {
@@ -105,13 +91,6 @@ bool CreateComplexDir(CFSTR _path)
 }
 
 
-
-bool MyGetFullPathName(CFSTR path, FString &resFullPath)
-{
-  return GetFullPath(path, resFullPath);
-}
-
-
 bool GetFullPathAndSplit(CFSTR path, FString &resDirPrefix, FString &resFileName)
 {
   bool res = MyGetFullPathName(path, resDirPrefix);
@@ -122,12 +101,6 @@ bool GetFullPathAndSplit(CFSTR path, FString &resDirPrefix, FString &resFileName
   resFileName = resDirPrefix.Ptr((unsigned)pos);
   resDirPrefix.DeleteFrom((unsigned)pos);
   return res;
-}
-
-bool GetOnlyDirPrefix(CFSTR path, FString &resDirPrefix)
-{
-  FString resFileName;
-  return GetFullPathAndSplit(path, resDirPrefix, resFileName);
 }
 
 
@@ -143,7 +116,7 @@ bool MyGetTempPath(FString &path)
 	return true;
   }
 
-  // By DOS convention, try %TMP% first.
+  // By DOS convention, try %TMP% second.
   s = getenv("TMP");
   if (NFind::DoesDirExist_FollowLink(s)) {
     path = s;
@@ -154,7 +127,7 @@ bool MyGetTempPath(FString &path)
   path = "." STRING_PATH_SEPARATOR;
   return true;
 
-#if 0
+  #if 0
   path = STRING_PATH_SEPARATOR "tmp";
   const char *s;
   if (NFind::DoesDirExist_FollowLink(path))
@@ -163,8 +136,9 @@ bool MyGetTempPath(FString &path)
     s = "." STRING_PATH_SEPARATOR;
   path = s;
   return true;  
-#endif
+  #endif
 }
+
 
 bool CreateTempFile2(CFSTR prefix, bool addRandom, AString &postfix, NIO::COutFile *outFile)
 {
@@ -222,22 +196,6 @@ bool CreateTempFile2(CFSTR prefix, bool addRandom, AString &postfix, NIO::COutFi
   return false;
 }
 
-#if 0 // unused
-bool CTempFile::Create(CFSTR prefix, NIO::COutFile *outFile)
-{
-  if (!Remove())
-  if (!Remove())
-    return false;
-  _path.Empty();
-  AString postfix;
-  if (!CreateTempFile2(prefix, false, postfix, outFile))
-    return false;
-  _path = prefix;
-  _path += postfix;
-  _mustBeDeleted = true;
-  return true;
-}
-#endif
 
 bool CTempFile::CreateRandomInTempFolder(CFSTR namePrefix, NIO::COutFile *outFile)
 {
@@ -257,6 +215,7 @@ bool CTempFile::CreateRandomInTempFolder(CFSTR namePrefix, NIO::COutFile *outFil
   return true;
 }
 
+
 bool CTempFile::Remove()
 {
   if (!_mustBeDeleted)
@@ -265,34 +224,19 @@ bool CTempFile::Remove()
   return !_mustBeDeleted;
 }
 
+
 bool CTempFile::MoveTo(CFSTR name, bool deleteDestBefore)
 {
-  // DWORD attrib = 0;
   if (deleteDestBefore)
   {
     if (NFind::DoesFileExist_Raw(name))
     {
-      // attrib = NFind::GetFileAttrib(name);
       if (!DeleteFileAlways(name))
         return false;
     }
   }
   DisableDeleting();
   return MyMoveFile(_path, name);
-  
-  /*
-  if (attrib != INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_READONLY))
-  {
-    DWORD attrib2 = NFind::GetFileAttrib(name);
-    if (attrib2 != INVALID_FILE_ATTRIBUTES)
-      SetFileAttrib(name, attrib2 | FILE_ATTRIBUTE_READONLY);
-  }
-  */
-}
-
-bool RemoveDir(CFSTR path)
-{
-  return (rmdir(path) == 0);
 }
 
 
@@ -326,77 +270,40 @@ static BOOL My_CopyFile(CFSTR oldFile, CFSTR newFile)
 bool MyMoveFile(CFSTR oldFile, CFSTR newFile)
 {
   int res = rename(oldFile, newFile);
+
   if (res == 0)
     return true;
-  if (errno != EXDEV) // (oldFile and newFile are not on the same mounted filesystem)
+
+  if (errno != EXDEV)
+    // (oldFile and newFile are not on the same mounted filesystem)
     return false;
 
   if (My_CopyFile(oldFile, newFile) == FALSE)
     return false;
-    
+
   struct stat info_file;
   res = stat(oldFile, &info_file);
   if (res != 0)
     return false;
 
-  /*
-  ret = chmod(dst,info_file.st_mode & g_umask.mask);
-  */
   return (unlink(oldFile) == 0);
-}
-
-
-bool CreateDir(CFSTR path)
-{
-  return (mkdir(path) == 0);
-}
-
-static bool CreateDir2(CFSTR path)
-{
-  return (mkdir(path) == 0);
-}
-
-
-bool DeleteFileAlways(CFSTR path)
-{
-  return (remove(path) == 0);
-}
-
-bool SetCurrentDir(CFSTR path)
-{
-  return (chdir(path) == 0);
 }
 
 
 bool GetCurrentDir(FString &path)
 {
-  path.Empty();
+  char s[PATH_MAX + 1];
+  char *res = getcwd(s, PATH_MAX);
 
-  #define MY_PATH_MAX  PATH_MAX
-  // #define MY_PATH_MAX  1024
-
-  char s[MY_PATH_MAX + 1];
-  char *res = getcwd(s, MY_PATH_MAX);
   if (res)
   {
+    path.Empty();
     path = fas2fs(s);
     return true;
   }
-  {
-    // if (errno != ERANGE) return false;
-    return false;
-  }
+
+  return false;
 }
-
-
-
-// #undef UTIME_OMIT // to debug
-
-#ifndef UTIME_OMIT
-  /* we can define UTIME_OMIT for debian and another systems.
-     Is it OK to define UTIME_OMIT to -2 here, if UTIME_OMIT is not defined? */
-  // #define UTIME_OMIT -2
-#endif
 
 
 bool SetDirTime(CFSTR path, const CFiTime *cTime, const CFiTime *aTime, const CFiTime *mTime)
@@ -431,7 +338,7 @@ bool SetDirTime(CFSTR path, const CFiTime *cTime, const CFiTime *aTime, const CF
      (owc_posix_time->tm_min  <<  5) |       /* 00000011111100000, minutes.          */
      (owc_posix_time->tm_sec  >>  2) ;       /* 00000000000011111, duo-seconds.      */
 
-#if 0
+  #if 0
   /*
    * The _dos_open() function always returns EACCES (errno 6) for directories
    * because the DOS platform lacks an API for changing directory metadata.
@@ -452,7 +359,7 @@ bool SetDirTime(CFSTR path, const CFiTime *cTime, const CFiTime *aTime, const CF
   _dos_close(owc_handle);
   return true;
 
-#else
+  #else
   /*
    * Interrupt 21h Function 57h does the same thing as _dos_setftime except
    * that is does not return an error if the argument is a directory.
@@ -460,57 +367,24 @@ bool SetDirTime(CFSTR path, const CFiTime *cTime, const CFiTime *aTime, const CF
    * 7-Zip tries to reset the mtime on all extracted items, so this method
    * is simpler and faster because it does not have open+close overheads. 
    */
-    union REGS registers;
-    memset(&registers, 0, sizeof(registers));
+  union REGS registers;
+  memset(&registers, 0, sizeof(registers));
 
-    registers.w.ax  = 0x5701;
-    registers.w.cx  = owc_dos_time;
-    registers.w.di  = owc_dos_date;
-    registers.x.esi = (unsigned)path;
+  registers.w.ax  = 0x5701;
+  registers.w.cx  = owc_dos_time;
+  registers.w.di  = owc_dos_date;
+  registers.x.esi = (unsigned)path;
 
-    int386(0x21, &registers, &registers);
+  int386(0x21, &registers, &registers);
 
-    if (registers.x.cflag & 1) {
-        return false;
-    }
-
-    return true;
-#endif
-}
-
-
-struct C_umask
-{
-  mode_t mask;
-
-  C_umask()
-  {
-    /* by security reasons we restrict attributes according
-       with process's file mode creation mask (umask) */
-    const mode_t um = umask(0); // octal :0022 is expected
-    mask = 0777 & (~um);        // octal: 0755 is expected
-    umask(um);  // restore the umask
-    // printf("\n umask = 0%03o mask = 0%03o\n", um, mask);
-    
-    // mask = 0777; // debug we can disable the restriction:
+  if (registers.x.cflag & 1) {
+      return false;
   }
-};
 
-static C_umask g_umask;
-
-// #define PRF(x) x;
-#define PRF(x)
-
-#define TRACE_SetFileAttrib(msg) \
-  PRF(printf("\nSetFileAttrib(%s, %x) : %s\n", (const char *)path, attrib, msg);)
-
-#define TRACE_chmod(s, mode) \
-  PRF(printf("\n chmod(%s, %o)\n", (const char *)path, (unsigned)(mode));)
-
-int my_chown(CFSTR path, uid_t owner, gid_t group)
-{
-  return 0;
+  return true;
+  #endif // 0
 }
+
 
 bool SetFileAttrib_PosixHighDetect(CFSTR path, DWORD attrib)
 {
@@ -520,6 +394,5 @@ bool SetFileAttrib_PosixHighDetect(CFSTR path, DWORD attrib)
   return _dos_setfileattr(path, attrib) == 0;
 }
 
-// #endif
 
 }}}

commit 32acaf99bb7976a9a24ef24a3d7c7ad339cb2292

Resolve compiler warning 128
W128: initializer for variable cannot be bypassed

This was caused by commit 89b1970410d70bf651f6bf8a6b8d1a7ebf6d2a77.

Closes: #21

CPP/DOS/PropVariant.cpp
@@ -400,12 +400,14 @@ int CPropVariant::Compare(const CPropVariant &a) throw()
       return -MyCompare(boolVal, a.boolVal);
 
     case VT_FILETIME:
+    {
       const int res = CompareFileTime(&filetime, &a.filetime);
       if (res != 0)
         return res;
       const unsigned v1 = Get_Ns100();
       const unsigned v2 = a.Get_Ns100();
       return MyCompare(v1, v2);
+    }
 
     case VT_BSTR:
       // Not implemented

CPP/DOS/PropVariantConv.cpp
@@ -165,9 +165,11 @@ void ConvertPropVariantToShortString(const PROPVARIANT &prop, char *dest) throw(
       return;
 
     case VT_FILETIME:
+    {
       int level = 0;
       ConvertUtcFileTimeToString(prop.filetime, dest, level);
       return;
+    }
 
 //  case VT_I1:
 //    ConvertInt64ToString(prop.cVal, dest);
@@ -229,9 +231,11 @@ void ConvertPropVariantToShortString(const PROPVARIANT &prop, wchar_t *dest) thr
       return;
 
     case VT_FILETIME:
+    {
       int level = 0;
       ConvertUtcFileTimeToString(prop.filetime, dest, level);
       return;
+    }
 
 //  case VT_I1:
 //    ConvertInt64ToString(prop.cVal, dest);